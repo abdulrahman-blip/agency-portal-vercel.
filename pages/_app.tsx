352e5f1e6cd388a613d1f4ed64532c0915145e5d
abdulrahman-blip <abdulrahman@io.net>
2025-10-01 07:08:18 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,6 @@
+import type { AppProps } from "next/app";
+import "../globals.css"; // عندك globals.css جاهز؛ لو نقلته لاحقاً حدّث المسار
 
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
